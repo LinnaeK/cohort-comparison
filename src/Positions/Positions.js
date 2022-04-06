1516a1329e1448626b30769b78979525377b8c65
linnaek <linnaekraemer@gmail.com>
2022-04-06 13:48:08 -0500

    Add Position Display

---
+++ src/Positions/Positions.js
@@ -0,0 +1,31 @@
+import RadioGroup from '../RadioGroup/RadioGroup.js'
+import { useState } from 'react'
+import './Positions.css'
+
+const Positions = function({selectedPosition, handleRadioClick, sportSelection}) {
+  const [positionOptions ] = useState({
+    'headCoach' : 'Head Coach',
+    'assistantCoach': 'Assistant Coach',
+    'referee' : 'Referee',
+    'assistant Referee':'Assistant Referee',
+    'player':'Player',
+    'leader':'Leader',
+  })
+
+  return (
+    <div className="positionContainer">
+      <RadioGroup 
+        label="Postions"
+        options={positionOptions}
+        value={selectedPosition}
+        handleRadioClick={(selection) => handleRadioClick(selection)}
+        displayNumber={sportSelection ? Object.keys(positionOptions).length: 0}
+      />
+      {!sportSelection ? <div className="instruction">
+        Select a Sport First
+      </div> : ''}
+    </div>
+  )
+}
+
+export default Positions
